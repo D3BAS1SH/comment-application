f70d4fb74c14854ddb2146c8b2317d133d586717
D3BAS1SH <Morning.Star00.111@protonmail.com>
2025-07-07 00:37:32 +0530

    feat(MicroService): Created 3 microservices directory.

---
+++ apps/comment-service/src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT');
+  console.log(`Comment-service is running on the port: ${port}`);
+  await app.listen(port ?? 3000);
+}
+bootstrap();
