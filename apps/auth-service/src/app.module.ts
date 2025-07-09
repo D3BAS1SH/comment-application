07d3f594db8bb874289c53761f2863a24d9e8cc1
D3BAS1SH <Morning.Star00.111@protonmail.com>
2025-07-09 12:17:48 +0530

    User and prisma module imported to the Main app module.

--- apps/auth-service/src/app.module.ts
+++ apps/auth-service/src/app.module.ts
@@ -1,16 +1,42 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal:true,
       envFilePath: '.env'
-    })
+    }),
+    ThrottlerModule.forRootAsync(
+      {
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: (config: ConfigService) => ({
+          throttlers:[
+            {
+              ttl: config.get<number>('THROTTLE_TTL',60),
+              limit: config.get<number>('THROTTLE_LIMIT',10)
+            }
+          ]
+        })
+      }
+    ),
+    PrismaModule,
+    UsersModule
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard
+    }
+  ],
 })
 export class AppModule {}
