3aa4c42cdf12149c769fcaa4d19bfb62642aecd5
D3BAS1SH <Morning.Star00.111@protonmail.com>
2025-07-09 12:26:16 +0530

    feat(security): CORS,Pipes,prefixes, PORT added.

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,12 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
+import { CorsOptions } from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  const port = configService.get<number>('PORT')
-  console.log(`Auth-service running on PORT: ${port}`)
+  const port = configService.get<number>('PORT');
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist:true,
+    forbidNonWhitelisted:true,
+    transform:true
+  }))
+
+  const allowedOrigins = configService.getOrThrow<string>("CORS_ORIGINS").split(',').map(orig=>orig.trim());
+  app.enableCors({
+    origin: allowedOrigins,
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    maxAge: 3600,
+    credentials: true
+  })
+
+  app.setGlobalPrefix('api/v1');
+
+  console.log(`Auth-service running on PORT: ${port}`);
   await app.listen(port ?? 3000);
 }
 bootstrap();
