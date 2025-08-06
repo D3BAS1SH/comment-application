989550c17a2ac741372d438a839436937d21b4f5
D3BAS1SH <Morning.Star00.111@protonmail.com>
2025-08-06 20:56:45 +0530

    fix(user): dto fixed the bug of exposure and exclusion.

--- apps/auth-service/src/users/dto/get-user-response.dto.ts
+++ apps/auth-service/src/users/dto/get-user-response.dto.ts
@@ -1,28 +1,35 @@
 import { IsBoolean, IsDate, IsEmail, IsOptional, IsString, IsUrl } from "class-validator";
-import { Exclude } from "class-transformer";
+import { Exclude, Expose } from "class-transformer";
 
 export class UserResponse{
     @IsString()
+    @Expose()
     id: string;
 
     @IsString()
+    @Expose()
     firstName: string;
 
     @IsString()
+    @Expose()
     lastName: string;
 
     @IsEmail()
+    @Expose()
     email: string;
     
     @IsOptional()
     @IsUrl()
+    @Expose()
     imageUrl?: string;
 
     @IsDate()
     @IsOptional()
+    @Expose()
     createdAt?: Date;
 
     @IsBoolean()
+    @Expose()
     isVerfied: boolean;
 
     @Exclude()
