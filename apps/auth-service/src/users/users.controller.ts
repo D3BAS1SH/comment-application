2c1a019a6810577beb6aca5c2d113c286e38eb6d
D3BAS1SH <Morning.Star00.111@protonmail.com>
2025-08-06 21:00:23 +0530

    feat(controller): cookies managing and login, logout route defined

--- apps/auth-service/src/users/users.controller.ts
+++ apps/auth-service/src/users/users.controller.ts
@@ -1,19 +1,28 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Query, Res, Req, UnauthorizedException, BadRequestException, UseInterceptors } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
-import { response } from 'express';
+import { Request, Response } from 'express';
 import { LoginUser } from './dto/login-user.dto';
+import { ConfigService } from '@nestjs/config';
+import * as ms from 'ms';
+import { ValidUserDto } from './dto/valid-user-payload.dto';
+import { CacheInterceptor } from '@nestjs/cache-manager';
 
 @Controller('users')
+@UseInterceptors(CacheInterceptor)
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService, private readonly configService: ConfigService) {}
 
   @Post('register')
   async registerUser(@Body() createUserDto: CreateUserDto) {
-    console.log(createUserDto);
-    return await this.usersService.create(createUserDto);
+    try {
+      console.log(createUserDto);
+      return await this.usersService.create(createUserDto);
+    } catch (error) {
+      throw error;
+    }
   }
 
   @Get()
@@ -29,12 +38,48 @@ export class UsersController {
   }
 
   @Post('login')
-  async login(@Body() loginUser:LoginUser){
+  async login(@Body() loginUser:LoginUser,@Res({passthrough: true}) response: Response) {
     try {
-      const response = await this.usersService.login(loginUser);
-      return response;
+      const loginResponse = await this.usersService.login(loginUser);
+      const expirationValue = this.configService.get('JWT_REFRESH_EXPIRATION');
+      const maxAgeMilisecondsRefreshToken = ms(expirationValue);
+      response.cookie('refreshToken', loginResponse.refreshToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'strict',
+        maxAge: maxAgeMilisecondsRefreshToken,
+        path: '/',
+      });
+      console.log("Cookie set with refresh Token");
+      return loginResponse;
     } catch (error) {
-      return error;
+      throw error;
+    }
+  }
+
+  @Post('logout')
+  @UseGuards(JwtAuthGuard)
+  async logoutUser(
+    @Req() request: Request,
+    @Res({passthrough: true}) response: Response
+  ):Promise<{message: string}>{
+    try {
+      const userPayload = request.user as ValidUserDto;
+      console.log(userPayload);
+      if(!userPayload){
+        throw new UnauthorizedException(`User is unauthenticated or lost validity or token is failed to verify`);
+      }
+      console.log(request.cookies);
+      const accessToken = request.headers.authorization?.replace('Bearer ', '');
+      if(!accessToken){
+        throw new BadRequestException('Authorization field incorrect or not sent at all');
+      }
+      console.log(accessToken);
+      const Result = await this.usersService.logout(userPayload, accessToken);
+      response.clearCookie('refreshToken')
+      return {message:Result ?? "Logging Out Successful"};
+    } catch (error) {
+      throw error;
     }
   }
 
