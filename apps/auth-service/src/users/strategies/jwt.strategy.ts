09b9102899fbe37888e746f35701c199e9d59af5
D3BAS1SH <Morning.Star00.111@protonmail.com>
2025-08-06 20:58:42 +0530

    feat(strategy): blacklist access token validation.

--- apps/auth-service/src/users/strategies/jwt.strategy.ts
+++ apps/auth-service/src/users/strategies/jwt.strategy.ts
@@ -1,10 +1,13 @@
-import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { Inject, Injectable, UnauthorizedException } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PrismaService } from "src/prisma/prisma.service";
 import { UserResponse } from "../dto/get-user-response.dto";
 import { plainToInstance } from "class-transformer";
+import { CACHE_MANAGER } from "@nestjs/cache-manager";
+import { Cache } from "cache-manager";
+import { ValidUserDto } from "../dto/valid-user-payload.dto";
 
 interface JwtPayload {
     sub: string;
@@ -18,36 +21,66 @@ interface JwtPayload {
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
     constructor(
         private configService: ConfigService,
-        private prisma: PrismaService
+        private prisma: PrismaService,
+        @Inject(CACHE_MANAGER) private readonly cacheManager: Cache
     ) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             ignoreExpiration: false,
-            secretOrKey: configService.get<string>('JWT_ACCESS_SECRET')!
-        })
+            secretOrKey: configService.get<string>('JWT_ACCESS_SECRET')!,
+            passReqToCallback: true
+        });
+        console.log('🔍 JwtStrategy - CacheManager injected:', !!this.cacheManager);
+        console.log('🔍 JwtStrategy - CacheManager type:', typeof this.cacheManager);
     }
 
-    async validate(payload: JwtPayload): Promise<UserResponse> {
-        const user = await this.prisma.user.findUnique({
-            where:{
-                id: payload.sub
+    /**
+     * Validates the JWT payload and retrieves the user information.
+     * @param req - The request object.
+     * @param payload - The JWT payload containing user information.
+     * @returns The validated user information.
+     */
+    async validate(req:any, payload: JwtPayload): Promise<ValidUserDto> {
+        try {
+            const accessToken = ExtractJwt.fromAuthHeaderAsBearerToken()(req);
+            if(!accessToken){
+                throw new UnauthorizedException("Token not found");
             }
-        })
-
-        if(!user){
-            throw new UnauthorizedException("User not found");
+            const tokenToCheck = `BL:${accessToken.trim()}`;
+            // console.log("token to check : ");
+            // console.log(tokenToCheck);
+            // console.log('=== END CACHE DEBUG ===');
+            const isBlacklisted = await this.cacheManager.get(tokenToCheck);
+            console.log(isBlacklisted);
+            if(isBlacklisted){
+                throw new UnauthorizedException("You are unauthorized to access as the token is revoked and you have been logged out");
+            }
+            console.log(`Is blacklited? : ${isBlacklisted}`);
+            
+            // console.log("Hitting prisma Query");
+            const user = await this.prisma.user.findUnique({
+                where:{
+                    id: payload.sub
+                }
+            })
+    
+            if(!user){
+                throw new UnauthorizedException("User not found");
+            }
+    
+            if(!user.isVerified){
+                throw new UnauthorizedException("User is not verified. Please Check your inbox or verify yourself.");
+            }
+    
+            if(user.deletedAt){
+                throw new UnauthorizedException("User is deleted or deactive");
+            }
+            return plainToInstance(ValidUserDto,{...user,...payload},{
+                excludeExtraneousValues:true
+            })
+        } catch (error) {
+            console.log(error);
+            throw new UnauthorizedException(error,"Something is wrong at the token level");
         }
-
-        if(!user.isVerified){
-            throw new UnauthorizedException("User is not verified. Please Check your inbox or verify yourself.");
-        }
-
-        if(user.deletedAt){
-            throw new UnauthorizedException("User is deleted or deactive");
-        }
-
-        return plainToInstance(UserResponse,user,{
-            excludeExtraneousValues:true
-        })
     }
 }
