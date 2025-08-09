e24bd26c1e01a526bb068fbcd8868651c0c905cb
D3BAS1SH <Morning.Star00.111@protonmail.com>
2025-08-10 00:47:00 +0530

    feat(email): Add forgot password mail template.

--- apps/auth-service/src/email/email.service.ts
+++ apps/auth-service/src/email/email.service.ts
@@ -35,4 +35,33 @@ export class EmailService {
             throw new InternalServerErrorException(error);
         }
     }
+
+    async sendForgotPasswordEmail(to: string, userName: string, token: string): Promise<void> {
+        try {
+            console.log("Sending Forgot Passwor Email Initiated");
+            const appBaseUrl = this.configService.get<string>('APP_BASE_URL','http://localhost:3033');
+            const verificationLink = `${appBaseUrl}/api/v1/users/reset-password-verify?token=${token}`;
+            const expirationMinutes = this.configService.get<number>('EMAIL_VERIFICATION_TOKEN_EXPIRATION_MINUTES',30);
+            console.log("Sending Verification Mail Link Generation.")
+
+            console.log("Sending Verification Mail Sending");
+            const responseEmailSent = await this.mailerService.sendMail({
+                to: to,
+                subject: 'Reset Password Verification',
+                template: 'forget-password',
+                context: {
+                    brand_name: 'Horizoncomms',
+                    name: userName,
+                    reset_url: verificationLink,
+                    expires_in_minutes: expirationMinutes,
+                    support_email: 'splinter.cell.1020@gmail.com',
+                    year: (new Date()).getFullYear()
+                }
+            })
+            console.log(responseEmailSent);
+        } catch (error) {
+            console.error(error);
+            throw new InternalServerErrorException(error,"Something went wrong while sending email.");
+        }
+    }
 }
