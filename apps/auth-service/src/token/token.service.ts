cef67bf7812d0eea3dbd3036208eb48595cd6931
D3BAS1SH <Morning.Star00.111@protonmail.com>
2025-08-09 20:21:45 +0530

    refactor(token.service): Exceptions handled by filters

--- apps/auth-service/src/token/token.service.ts
+++ apps/auth-service/src/token/token.service.ts
@@ -1,13 +1,13 @@
-import { BadRequestException, Injectable, UnauthorizedException, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, UnauthorizedException, InternalServerErrorException, NotFoundException, ForbiddenException } from '@nestjs/common';
 import { CreateTokenDto } from './dto/create-token.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { JwtService, TokenExpiredError } from '@nestjs/jwt';
+import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 import { User } from '@prisma/client';
-import { RefreshTokenDto } from './dto/create-refresh-token.dto';
 import { v4 as uuidv4 } from 'uuid';
 import { VerificationTokenDto } from './dto/verification-token.dto';
 import { VerificationTokenResponse } from './dto/verification-token-response.dto';
+import { RefreshTokenDto } from './dto/create-refresh-token.dto';
 
 @Injectable()
 export class TokenService {
@@ -29,54 +29,48 @@ export class TokenService {
    */
   async generateTokens(user: User): Promise<CreateTokenDto> {
 
-    try {
-      //Checking the number of Refresh Tokens the user have.(Revoke if, Already logged in more than 3 devices.)
-      const tokenNumbers = await this.prisma.token.findMany({
-        where:{
-          ownerId: user.id
-        },
-        select:{
-          id:true
-        }
-      })
-  
-      if(tokenNumbers.length>=3){
-        throw new BadRequestException('Exceeding Device limits.');
+    //Checking the number of Refresh Tokens the user have.(Revoke if, Already logged in more than 3 devices.)
+    const tokenNumbers = await this.prisma.token.findMany({
+      where:{
+        ownerId: user.id
+      },
+      select:{
+        id:true
       }
-  
-      //Extracting informations as needed for payload
-      const uniqueSessionToken = uuidv4();
-      const payload:tokenPayload = { sub: user.id, email: user.email, isVerified: user.isVerified,sessionToken: uniqueSessionToken };
-  
-      //Generate the Access Token
-      const accessToken = this.jwtService.sign(payload,{
-        secret: this.configService.get<string>('JWT_ACCESS_SECRET'),
-        expiresIn: this.configService.get<string>('JWT_ACCESS_EXPIRATION')
-      })
-  
-      //Generate the Refresh Token
-      const refreshToken = this.jwtService.sign(payload,{
-        secret: this.configService.get<string>('JWT_REFRESH_SECRET'),
-        expiresIn: this.configService.get<string>('JWT_REFRESH_EXPIRATION')
-      })
-  
-      //Saving Token for the user
-      await this.prisma.token.create({
-        data:{
-          refreshToken: refreshToken,
-          ownerId: user.id,
-          sessionTokenId: uniqueSessionToken
-        }
-      })
-  
-      return {
-        accessToken,
-        refreshToken
+    })
+
+    if(tokenNumbers.length>=3){
+      throw new BadRequestException('Exceeding Device limits.');
+    }
+
+    //Extracting informations as needed for payload
+    const uniqueSessionToken = uuidv4();
+    const payload:tokenPayload = { sub: user.id, email: user.email, isVerified: user.isVerified,sessionToken: uniqueSessionToken };
+
+    //Generate the Access Token
+    const accessToken = this.jwtService.sign(payload,{
+      secret: this.configService.get<string>('JWT_ACCESS_SECRET'),
+      expiresIn: this.configService.get<string>('JWT_ACCESS_EXPIRATION')
+    })
+
+    //Generate the Refresh Token
+    const refreshToken = this.jwtService.sign(payload,{
+      secret: this.configService.get<string>('JWT_REFRESH_SECRET'),
+      expiresIn: this.configService.get<string>('JWT_REFRESH_EXPIRATION')
+    })
+
+    //Saving Token for the user
+    await this.prisma.token.create({
+      data:{
+        refreshToken: refreshToken,
+        ownerId: user.id,
+        sessionTokenId: uniqueSessionToken
       }
-    } catch (error) {
-      console.warn("Some error occured while generating token");
-      console.error(error);
-      throw new InternalServerErrorException(error);
+    })
+
+    return {
+      accessToken,
+      refreshToken
     }
   }
 
@@ -89,36 +83,66 @@ export class TokenService {
    * @throws UnauthorizedException - If the refresh token is expired or invalid.
    * @throws InternalServerErrorException - If an error occurs during the process or if the token is invalid or broken signature.
    */
-  async refreshToken(refreshToken: string): Promise<CreateTokenDto> {
-    try {
+  async refreshToken(refreshToken: string,payload: tokenPayload): Promise<CreateTokenDto> {
+    console.log("----------------Token Service Refresh Debug-------------")
+    console.log(payload);
+    console.log("------------------Token Service END Debug---------------")
 
-      const payload = await this.jwtService.verifyAsync<tokenPayload>(refreshToken,{
-        secret: this.configService.getOrThrow<string>('JWT_REFRESH_SECRET')
-      });
+    //New payload setup
+    const uniqueSessionToken = uuidv4();
+    const newPayload:tokenPayload = {
+      email:payload.email,
+      isVerified:payload.isVerified,
+      sessionToken:uniqueSessionToken,
+      sub:payload.sub
+    };
 
-      const newAccessToken = await this.jwtService.signAsync(payload,{
-        secret: this.configService.getOrThrow<string>('JWT_ACCESS_SECRET'),
-        expiresIn: this.configService.getOrThrow<string>('JWT_ACCESS_EXPIRATION')
+    const newAccessToken = await this.jwtService.signAsync(newPayload,{
+      secret: this.configService.getOrThrow<string>('JWT_ACCESS_SECRET'),
+      expiresIn: this.configService.getOrThrow<string>('JWT_ACCESS_EXPIRATION')
+    })
+
+    const newRefreshToken = await this.jwtService.signAsync(newPayload,{
+      secret: this.configService.getOrThrow<string>('JWT_REFRESH_SECRET')!,
+      expiresIn: this.configService.getOrThrow<string>('JWT_REFRESH_EXPIRATION')
+    });
+
+    const transactionResult = await this.prisma.$transaction(async (tx) => {
+      // await this.invalidateRefreshTokenBySessionToken(payload.sessionToken,payload.sub,tx);
+      
+      const truthOfToken = await tx.token.findUnique({
+        where:{
+          refreshToken: refreshToken
+        }
       })
 
-      return {
-        accessToken: newAccessToken,
-        refreshToken: refreshToken
-      };
-    } catch (error) {
-      console.warn("Some error occured while refreshing token.");
-      console.error(error);
-
-      if(error instanceof TokenExpiredError){
-        console.log("Invalidation Initiated");
-        await this.invalidateRefreshToken({refreshToken});
-        console.log("Invalidation completed");
-
-        throw new UnauthorizedException('Refresh token has expired. Please login again.');
+      if(!truthOfToken || truthOfToken.ownerId !== payload.sub){
+        throw new ForbiddenException('Access Denied: Invalid Refresh Token');
       }
 
-      throw new InternalServerErrorException("Refresh Token is invalid or expired.");
-    }
+      await tx.token.delete({
+        where:{
+          refreshToken: refreshToken,
+          sessionTokenId: payload.sessionToken,
+          ownerId: payload.sub
+        }
+      })
+
+      await tx.token.create({
+        data:{
+          refreshToken:newRefreshToken,
+          sessionTokenId: uniqueSessionToken,
+          ownerId: payload.sub
+        }
+      });
+    });
+
+    console.log("Refresh Token saved to database.");
+
+    return {
+      accessToken: newAccessToken,
+      refreshToken: newRefreshToken
+    };
   }
 
   /**
@@ -148,23 +172,18 @@ export class TokenService {
   }
 
   async invalidateRefreshTokenBySessionToken(sessionToken: string, userId: string):Promise<void>{
-    try {
-      if(!sessionToken){
-        throw new BadRequestException("session token is required.");
-      }
-      
-      await this.prisma.token.delete({
-        where:{
-          sessionTokenId: sessionToken,
-          ownerId: userId
-        }
-      });
-
-      console.log("Refresh Token Removed");
-    } catch (error) {
-      console.error(error);
-      throw new InternalServerErrorException(error,"Something went wrong while deleting the refresh token");
+    if(!sessionToken){
+      throw new BadRequestException("session token is required.");
     }
+    
+    await this.prisma.token.delete({
+      where:{
+        sessionTokenId: sessionToken,
+        ownerId: userId
+      }
+    });
+
+    console.log("Refresh Token Removed");
   }
 
   /**
@@ -177,18 +196,13 @@ export class TokenService {
    */
   async invalidateAllTokens(userId: string): Promise<void> {
     //Remove all tokens of the user.
-    try {
-      await this.prisma.token.deleteMany({
-        where:{
-          ownerId: userId
-        }
-      })
-  
-      console.log("Successful revocation of all tokens successful.");
-    } catch (error) {
-      console.warn("Some error occured while invalidating Al tokens of a specific user");
-      throw new InternalServerErrorException(error);
-    }
+    await this.prisma.token.deleteMany({
+      where:{
+        ownerId: userId
+      }
+    })
+
+    console.log("Successful revocation of all tokens successful.");
   }
 
   /**
@@ -199,40 +213,35 @@ export class TokenService {
    * @returns A promise that resolves to the newly created verification token.
    */
   async createVerificationToken(userId: string,tx?: any): Promise<VerificationTokenDto> {
-    try {
-      const token = uuidv4();
-      const expirationMinutes = this.configService.get<number>('EMAIL_VERIFICATION_TOKEN_EXPIRATION_MINUTES',30);
-      const expiresAt = new Date(Date.now() + expirationMinutes * 60 * 1000); 
-  
-      const currentPrismaClient = tx || this.prisma;
-      
-      //Removing all verificationTokens linked to the user
-      await currentPrismaClient.verificationToken.deleteMany({
-        where:{
-          userId:userId
-        }
-      })
-      console.log("Removed all verification token of a user");
-  
-      //Token Creating in database.
-      await currentPrismaClient.verificationToken.create({
-        data:{
-          token: token,
-          userId: userId,
-          expiresAt: expiresAt
-        }
-      })
-  
-      return {
-        expiresAt: expiresAt,
+
+    const token = uuidv4();
+    const expirationMinutes = this.configService.get<number>('EMAIL_VERIFICATION_TOKEN_EXPIRATION_MINUTES',30);
+    const expiresAt = new Date(Date.now() + expirationMinutes * 60 * 1000); 
+
+    const currentPrismaClient = tx || this.prisma;
+    
+    //Removing all verificationTokens linked to the user
+    await currentPrismaClient.verificationToken.deleteMany({
+      where:{
+        userId:userId
+      }
+    })
+    console.log("Removed all verification token of a user");
+
+    //Token Creating in database.
+    await currentPrismaClient.verificationToken.create({
+      data:{
         token: token,
-        userId: userId
-      };
-    } catch (error) {
-      console.warn("Some error ")
-      console.error(error);
-      throw new InternalServerErrorException(error);
-    }
+        userId: userId,
+        expiresAt: expiresAt
+      }
+    })
+
+    return {
+      expiresAt: expiresAt,
+      token: token,
+      userId: userId
+    };
   }
 
   //Verification methods
@@ -254,80 +263,64 @@ export class TokenService {
  * @throws InternalServerErrorException - For any unexpected error during the process.
  */
   async verifyVerificationToken(token: string): Promise<VerificationTokenResponse> {
-    try {
-      const verificationRecord = await this.prisma.verificationToken.findUnique({
-        where:{
-          token
-        },
-        include:{
-          user: true
-        }
-      })
-  
-      if(!verificationRecord){
-        throw new NotFoundException('Verification Token not found or invalid');
-      }
-  
-      if(verificationRecord.expiresAt < new Date()){
-        const finalResultOfPurge = await this.prisma.$transaction(async (tx)=> {
 
-          try {
-            const deleteTheExpiredToken = await tx.verificationToken.delete({
-              where:{
-                userId: verificationRecord.userId,
-                token: token
-              }
-            })
-            if(!deleteTheExpiredToken){
-              throw new InternalServerErrorException("Something went wrong while deleting the token.")
-            }
-            const userDeletion = await tx.user.delete({
-              where:{id:verificationRecord.userId}
-            })
-            if(!userDeletion){
-              throw new InternalServerErrorException("Something went wrong while deleting the user document.")
-            }
-            return "Completed Deletion of token and user."
-          } catch (error) {
-            console.warn("Error at transaction of user and token deletion");
-            console.error(error);
-            throw new InternalServerErrorException(error);
+    const verificationRecord = await this.prisma.verificationToken.findUnique({
+      where:{
+        token
+      },
+      include:{
+        user: true
+      }
+    })
+
+    if(!verificationRecord){
+      throw new NotFoundException('Verification Token not found or invalid');
+    }
+
+    if(verificationRecord.expiresAt < new Date()){
+      const finalResultOfPurge = await this.prisma.$transaction(async (tx)=> {
+
+        await tx.verificationToken.delete({
+          where:{
+            userId: verificationRecord.userId,
+            token: token
           }
-
         })
-        console.log(finalResultOfPurge);
-        throw new UnauthorizedException('Verification Token has expired');
-      }
-  
-      const updateUser = await this.prisma.user.update({
-        where: {
-          id:verificationRecord.userId
-        },
-        data: {
-          isVerified: true
-        },
-        omit:{
-          password: true,
-          updatedAt: true,
-          deletedAt: true
-        }
+
+        await tx.user.delete({
+          where:{id:verificationRecord.userId}
+        })
+
+        return "Completed Deletion of token and user."
       })
-  
-      await this.prisma.verificationToken.delete({
-        where:{
-          id: verificationRecord.id
-        }
-      })
-  
-      return {
-        email: updateUser.email,
-        isVerified: updateUser.isVerified,
-        message: `The user with the email id ${updateUser.isVerified} is verified`
+      console.log(finalResultOfPurge);
+      throw new UnauthorizedException('Verification Token has expired');
+    }
+
+    const updateUser = await this.prisma.user.update({
+      where: {
+        id:verificationRecord.userId
+      },
+      data: {
+        isVerified: true
+      },
+      omit:{
+        password: true,
+        updatedAt: true,
+        deletedAt: true
       }
-    } catch (error) {
-      console.warn("Error Occured due to unknown reason");
-      console.error(error);
-      throw new InternalServerErrorException(error);
+    })
+
+    await this.prisma.verificationToken.delete({
+      where:{
+        id: verificationRecord.id
+      }
+    })
+
+    return {
+      email: updateUser.email,
+      isVerified: updateUser.isVerified,
+      message: `The user with the email id ${updateUser.isVerified} is verified`
     }
   }
 
