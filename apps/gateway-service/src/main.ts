ea0ba247213bc51b1d5ec19fb0d4d8546ab2076c
D3BAS1SH <Morning.Star00.111@protonmail.com>
2025-08-18 16:55:22 +0530

    refactor(app): update PORT to GATEWAY_PORT.

--- apps/gateway-service/src/main.ts
+++ apps/gateway-service/src/main.ts
@@ -1,3 +1,4 @@
+import "reflect-metadata";
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
@@ -9,7 +10,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const currentConfigService = app.get(ConfigService);
-  const port = currentConfigService.getOrThrow<number>('PORT');
+  const port = currentConfigService.getOrThrow<number>('GATEWAY_PORT');
 
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
