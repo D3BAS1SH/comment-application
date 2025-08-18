97d526f0f9efe1edff2193a9873cc27e96b1f8c7
D3BAS1SH <Morning.Star00.111@protonmail.com>
2025-08-18 13:40:25 +0530

    feat(filter): Add hanlder Module, Exception Filters.

--- apps/gateway-service/src/app.module.ts
+++ apps/gateway-service/src/app.module.ts
@@ -2,15 +2,35 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { GatewayHandlerModule } from './gateway-handler/gateway-handler.module';
+import { APP_FILTER } from '@nestjs/core';
+import { JwtExceptionFilter } from './common/filters/jwt-exception.filter';
+import { PrismaClientExceptionFilter } from './common/filters/prisma-exception.filter';
+import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env'
-    })
+    }),
+    GatewayHandlerModule
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: JwtExceptionFilter
+    },
+    {
+      provide: APP_FILTER,
+      useClass: PrismaClientExceptionFilter
+    },
+    {
+      provide: APP_FILTER,
+      useClass: GlobalExceptionFilter
+    }
+  ],
 })
 export class AppModule {}
