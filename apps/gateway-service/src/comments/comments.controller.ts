17a0345021f291b888ee720391e8ef7535442309
D3BAS1SH <Morning.Star00.111@protonmail.com>
2025-08-18 18:25:59 +0530

    refactor(comment,post): implement guards in comments and posts controller.

--- apps/gateway-service/src/comments/comments.controller.ts
+++ apps/gateway-service/src/comments/comments.controller.ts
@@ -1,8 +1,10 @@
 import { All, Controller, UseGuards } from '@nestjs/common';
 import { Throttle, ThrottlerGuard } from '@nestjs/throttler';
 import { CommentsService } from './comments.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @UseGuards(ThrottlerGuard)
+@UseGuards(JwtAuthGuard)
 @Controller('comments')
 export class CommentsController {
     constructor(private readonly commentsService: CommentsService){}
--- apps/gateway-service/src/posts/posts.controller.ts
+++ apps/gateway-service/src/posts/posts.controller.ts
@@ -1,8 +1,10 @@
 import { All, Controller, UseGuards } from '@nestjs/common';
 import { Throttle, ThrottlerGuard } from '@nestjs/throttler';
 import { PostsService } from './posts.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @UseGuards(ThrottlerGuard)
+@UseGuards(JwtAuthGuard)
 @Controller('posts')
 export class PostsController {
     constructor(private readonly postService: PostsService){}
