790db74332e908b47c637ea75cfad4b5f4f493e5
D3BAS1SH <Morning.Star00.111@protonmail.com>
2025-08-18 16:46:18 +0530

    refactor: update name of PORT to AUTH_PORT

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
     logger: ["debug","error","fatal","log","warn","verbose"]
   });
   const configService = app.get(ConfigService);
-  const port = configService.get<number>('PORT');
+  const port = configService.get<number>('AUTH_PORT');
 
   app.useGlobalPipes(new ValidationPipe({
     whitelist:true,
