aedd19bc7fdcef71cf8ce169d177c0ef7493afd1
D3BAS1SH <Morning.Star00.111@protonmail.com>
2026-03-17 14:28:34 +0530

    refactor: create a centralized server configurations and add them to clients

--- apps/frontend/src/server/http/clients.ts
+++ apps/frontend/src/server/http/clients.ts
@@ -1,27 +1,26 @@
+import {
+  AUTH_SERVICE_URL,
+  CHAT_SERVICE_URL,
+  POSTS_SERVICE_URL,
+  PROFILE_SERVICE_URL,
+  WORKSPACE_SERVICE_URL,
+} from '../config/server-configs';
 import { createServiceClient } from './create-service-client';
+
 // Each client points to a microservice via Nginx
 
-export const authClient = createServiceClient(
-  process.env.AUTH_SERVICE_URL!, // internal URL, not exposed to browser
-  'auth'
-);
+export const authClient = createServiceClient(AUTH_SERVICE_URL, 'auth');
 
-export const postsClient = createServiceClient(
-  process.env.POSTS_SERVICE_URL!,
-  'posts'
-);
+export const postsClient = createServiceClient(POSTS_SERVICE_URL, 'posts');
 
-export const chatClient = createServiceClient(
-  process.env.CHAT_SERVICE_URL!,
-  'chat'
-);
+export const chatClient = createServiceClient(CHAT_SERVICE_URL, 'chat');
 
 export const workspaceClient = createServiceClient(
-  process.env.WORKSPACE_SERVICE_URL!,
+  WORKSPACE_SERVICE_URL,
   'workspace'
 );
 
 export const profileClient = createServiceClient(
-  process.env.PROFILE_SERVICE_URL!,
+  PROFILE_SERVICE_URL,
   'profile'
 );
