d67c6724efa8c24b82180a1fb5dc8f47ec529076
D3BAS1SH <Morning.Star00.111@protonmail.com>
2026-03-26 22:01:58 +0530

    feat: add logger interceptor

---
+++ apps/task-oper-service/src/common/interceptors/logging.interceptor.ts
@@ -0,0 +1,26 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from "@nestjs/common";
+import { Observable, tap } from "rxjs";
+import { LoggerService } from "../logger/logger.service";
+import { Request } from "express";
+
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+
+    constructor(private readonly loggerService: LoggerService){}
+
+    intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+        const req = context.switchToHttp().getRequest<Request>();
+        const { method, url } = req;
+        
+        const startTime = Date.now();
+
+        this.loggerService.log(`[REQUEST] ${method} ${url}`, 'Logging Interceptor');
+
+        return next.handle().pipe(
+            tap(()=> {
+                const timeTaken = Date.now() - startTime;
+                this.loggerService.log(`[RESPONSE] ${method} ${url} - ${timeTaken}ms`, 'Logging Interceptor');
+            })
+        )
+    }
+}
