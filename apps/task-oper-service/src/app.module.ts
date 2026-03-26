6d21e1bf7f913575517fdad109701087701466e9
D3BAS1SH <Morning.Star00.111@protonmail.com>
2026-03-26 22:03:09 +0530

    feat: implement logger middleware service interceptor

--- apps/task-oper-service/src/app.module.ts
+++ apps/task-oper-service/src/app.module.ts
@@ -1,10 +1,39 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerModule } from './common/logger/logger.module';
+import { TraceMiddleware } from './common/logger/trace.middleware';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
+import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
+import { PrismaClientExceptionFilter } from './common/filters/prisma-exception.filter';
 
 @Module({
-  imports: [],
+  imports: [
+    LoggerModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    // 1. Performance Monitor (Runs first in the pipeline)
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor
+    },
+    // 2. Specific Exception Filters (More specific first)
+    {
+      provide: APP_FILTER,
+      useClass: PrismaClientExceptionFilter,
+    },
+    // 3. Global Catch-All Filter (Must be last)
+    {
+      provide: APP_FILTER,
+      useClass: GlobalExceptionFilter,
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(TraceMiddleware).forRoutes('*');
+  }
+}
