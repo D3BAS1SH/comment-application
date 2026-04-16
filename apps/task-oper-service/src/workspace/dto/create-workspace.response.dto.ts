b01939615529e7e06df1cca2701cb58e24866ae1
D3BAS1SH <Morning.Star00.111@protonmail.com>
2026-04-16 15:14:53 +0530

    style: format the indentations

--- apps/task-oper-service/src/workspace/dto/create-workspace.response.dto.ts
+++ apps/task-oper-service/src/workspace/dto/create-workspace.response.dto.ts
@@ -1,15 +1,21 @@
 export class CreateWorkspaceResponse {
-    id: string;
-    name: string;
-    slug: string;
-    createdAt: Date;
-    ownerId: string;
+  id: string;
+  name: string;
+  slug: string;
+  createdAt: Date;
+  ownerId: string;
 
-    constructor(id: string, name: string, slug: string, createdAt: Date, ownerId: string) {
-        this.id = id;
-        this.name = name;
-        this.slug = slug;
-        this.createdAt = createdAt;
-        this.ownerId = ownerId;
-    }
+  constructor(
+    id: string,
+    name: string,
+    slug: string,
+    createdAt: Date,
+    ownerId: string
+  ) {
+    this.id = id;
+    this.name = name;
+    this.slug = slug;
+    this.createdAt = createdAt;
+    this.ownerId = ownerId;
+  }
 }
--- apps/task-oper-service/src/workspace/dto/workspace-detail.dto.ts
+++ apps/task-oper-service/src/workspace/dto/workspace-detail.dto.ts
@@ -1,35 +1,35 @@
 export class WorkspaceDetails {
-    id: string;
-    name: string;
-    slug: string;
-    createdAt: Date;
-    ownerId: string;
-    owner: {
-        firstName: string;
-        email: string;
+  id: string;
+  name: string;
+  slug: string;
+  createdAt: Date;
+  ownerId: string;
+  owner: {
+    firstName: string;
+    email: string;
+  };
+  workspaceMembers: {
+    user: {
+      firstName: string;
+      email: string;
     };
-    workspaceMembers: {
-        user: {
-            firstName: string;
-            email: string;
-        }
-    }[];
+  }[];
 
-    constructor(
-        id: string, 
-        name: string, 
-        slug: string,
-        createdAt: Date, 
-        ownerId: string, 
-        owner: { firstName: string; email: string; }, 
-        workspaceMembers: { user: { firstName: string; email: string; } }[]
-    ) {
-        this.id = id;
-        this.name = name;
-        this.slug = slug;
-        this.createdAt = createdAt;
-        this.ownerId = ownerId;
-        this.owner = owner;
-        this.workspaceMembers = workspaceMembers;
-    }
+  constructor(
+    id: string,
+    name: string,
+    slug: string,
+    createdAt: Date,
+    ownerId: string,
+    owner: { firstName: string; email: string },
+    workspaceMembers: { user: { firstName: string; email: string } }[]
+  ) {
+    this.id = id;
+    this.name = name;
+    this.slug = slug;
+    this.createdAt = createdAt;
+    this.ownerId = ownerId;
+    this.owner = owner;
+    this.workspaceMembers = workspaceMembers;
+  }
 }
