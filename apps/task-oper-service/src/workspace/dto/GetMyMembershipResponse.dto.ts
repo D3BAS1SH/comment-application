e7fe972a10e29622adf1648ae7d752fdb19be65d
D3BAS1SH <Morning.Star00.111@protonmail.com>
2026-05-13 15:20:59 +0530

    feat: add membership dto

--- apps/task-oper-service/src/workspace/dto/GetMyMembershipResponse.dto.ts
+++ apps/task-oper-service/src/workspace/dto/GetMyMembershipResponse.dto.ts
@@ -1,9 +1,11 @@
-export class GetMyMembershipResponse {
+export class GetMembershipResponse {
     userId: string;
+    email: string;
     role: string;
 
-    constructor(userId: string, role: string) {
+    constructor(userId: string, role: string, email: string) {
         this.userId = userId;
         this.role = role;
+        this.email = email;
     }
 }
