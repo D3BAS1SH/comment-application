4315cbd410a2b68d658f9b7813e8e9c31ad99707
D3BAS1SH <Morning.Star00.111@protonmail.com>
2026-05-16 11:55:14 +0530

    feat: add controller end points

--- apps/task-oper-service/src/epic/epic.controller.ts
+++ apps/task-oper-service/src/epic/epic.controller.ts
@@ -1,4 +1,68 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { UserId } from 'src/common/decorators/UserId.decorator.js';
+import { CreateEpicDto } from './dtos/create-epic.dto.js';
+import { LoggerService } from 'src/common/logger/logger.service.js';
+import { EpicService } from './epic.service.js';
 
-@Controller('epic')
-export class EpicController {}
+@Controller('projects/:projectId/epics')
+export class EpicController {
+    private readonly context: string = EpicController.name;
+
+    constructor(
+        private readonly loggerService: LoggerService,
+        private readonly epicService: EpicService
+    ){}
+
+    @Post()
+    async createEpic(
+        @UserId() callerId: string,
+        @Param('projectId') projectId: string,
+        @Body() creatEpic: CreateEpicDto
+    ){
+        this.loggerService.log(`Create Epic by caller: ${callerId}`, this.context);
+    }
+
+    @Get()
+    async getEpics(
+        @UserId() callerId: string,
+        @Param('projectId') projectId: string,
+    ){
+        this.loggerService.log(`Get All Epic by caller: ${callerId}`, this.context);
+    }
+
+    @Get(':epicId')
+    async getEpic(
+        @UserId() callerId: string,
+        @Param('projectId') projectId: string,
+        @Param('epicId') epicId: string
+    ){
+        this.loggerService.log(`Get Specific Epic by caller: ${callerId}`, this.context);
+    }
+
+    @Get(':epicId/issues')
+    async getIssusesInEpic(
+        @UserId() callerId: string,
+        @Param('projectId') projectId: string,
+        @Param('epicId') epicId: string
+    ){
+        this.loggerService.log(`Get Issues Of Epic by caller: ${callerId}`, this.context);
+    }
+
+    @Patch(':epicId')
+    async updateEpic(
+        @UserId() callerId: string,
+        @Param('projectId') projectId: string,
+        @Param('epicId') epicId: string
+    ){
+        this.loggerService.log(`Update Epic by caller: ${callerId}`, this.context);
+    }
+
+    @Delete(':epicId')
+    async deleteEpic(
+        @UserId() callerId: string,
+        @Param('projectId') projectId: string,
+        @Param('epicId') epicId: string
+    ){
+        this.loggerService.log(`Delete Epic by caller: ${callerId}`, this.context);
+    }
+}
