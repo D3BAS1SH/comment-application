fe280a4a4258a435ab068fc661149b0a5b3414ed
D3BAS1SH <Morning.Star00.111@protonmail.com>
2026-05-16 13:16:13 +0530

    feat: integrate service to respected controller end point

--- apps/task-oper-service/src/epic/epic.controller.ts
+++ apps/task-oper-service/src/epic/epic.controller.ts
@@ -3,16 +3,20 @@ import {
   Controller,
   Delete,
   Get,
+  HttpCode,
+  HttpStatus,
   Param,
   Patch,
   Post,
 } from '@nestjs/common';
 import { UserId } from 'src/common/decorators/UserId.decorator.js';
 import { CreateEpicDto } from './dtos/create-epic.dto.js';
-import { LoggerService } from 'src/common/logger/logger.service.js';
+import { UpdateEpicDto } from './dtos/update-epic.dto.js';
 import { EpicService } from './epic.service.js';
 import { ApiResponse } from 'src/common/dto/api-response.dto.js';
 import { EpicResponseDto } from './dtos/epic-respones.dto.js';
+import { EpicListResponseDto } from './dtos/epic-list-response.dto.js';
+import { LoggerService } from 'src/common/logger/logger.service.js';
 
 @Controller('projects/:projectId/epics')
 export class EpicController {
@@ -38,8 +42,10 @@ export class EpicController {
   async getEpics(
     @UserId() callerId: string,
     @Param('projectId') projectId: string
-  ) {
+  ): Promise<ApiResponse<EpicListResponseDto>> {
     this.loggerService.log(`Get All Epic by caller: ${callerId}`, this.context);
+    const epics = await this.epicService.getEpics(callerId, projectId);
+    return ApiResponse.success(epics, 'Epics retrieved successfully');
   }
 
   @Get(':epicId')
@@ -47,11 +53,13 @@ export class EpicController {
     @UserId() callerId: string,
     @Param('projectId') projectId: string,
     @Param('epicId') epicId: string
-  ) {
+  ): Promise<ApiResponse<EpicResponseDto>> {
     this.loggerService.log(
       `Get Specific Epic by caller: ${callerId}`,
       this.context
     );
+    const epic = await this.epicService.getEpic(callerId, projectId, epicId);
+    return ApiResponse.success(epic, 'Epic retrieved successfully');
   }
 
   @Get(':epicId/issues')
@@ -59,28 +67,36 @@ export class EpicController {
     @UserId() callerId: string,
     @Param('projectId') projectId: string,
     @Param('epicId') epicId: string
-  ) {
+  ): Promise<ApiResponse<unknown>> {
     this.loggerService.log(
       `Get Issues Of Epic by caller: ${callerId}`,
       this.context
     );
+    const issues = await this.epicService.getIssuesInEpic(callerId, projectId, epicId);
+    return ApiResponse.success(issues, 'Issues retrieved successfully');
   }
 
   @Patch(':epicId')
   async updateEpic(
     @UserId() callerId: string,
     @Param('projectId') projectId: string,
-    @Param('epicId') epicId: string
-  ) {
+    @Param('epicId') epicId: string,
+    @Body() updateEpicObject: UpdateEpicDto
+  ): Promise<ApiResponse<EpicResponseDto>> {
     this.loggerService.log(`Update Epic by caller: ${callerId}`, this.context);
+    const updatedEpic = await this.epicService.updateEpic(callerId, projectId, epicId, updateEpicObject);
+    return ApiResponse.success(updatedEpic, 'Epic updated successfully');
   }
 
   @Delete(':epicId')
+  @HttpCode(HttpStatus.NO_CONTENT)
   async deleteEpic(
     @UserId() callerId: string,
     @Param('projectId') projectId: string,
     @Param('epicId') epicId: string
-  ) {
+  ): Promise<ApiResponse<null>> {
     this.loggerService.log(`Delete Epic by caller: ${callerId}`, this.context);
+    await this.epicService.deleteEpic(callerId, projectId, epicId);
+    return ApiResponse.success(null,'Epic deleted successfully');
   }
 }
