c2c03f7b07bdfa28430a62eb7d0fe6f6a915e438
D3BAS1SH <Morning.Star00.111@protonmail.com>
2026-05-16 14:25:57 +0530

    feat: add module end points

--- apps/task-oper-service/src/sprint/sprint.controller.ts
+++ apps/task-oper-service/src/sprint/sprint.controller.ts
@@ -1,4 +1,92 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { LoggerService } from 'src/common/logger/logger.service.js';
+import { SprintService } from './sprint.service.js';
+import { UserId } from 'src/common/decorators/UserId.decorator.js';
+import { CreateSprintDto } from './dtos/create-sprint.dto.js';
+import { UpdateSprintDto } from './dtos/update-sprint.dto.js';
+import { StartSprintDto } from './dtos/start-sprint.dto.js';
+import { CompleteSprintDto } from './dtos/complete-sprint.dto.js';
 
-@Controller('sprint')
-export class SprintController {}
+@Controller('projects/:projectId/sprints')
+export class SprintController {
+  private readonly context: string = SprintController.name;
+
+	constructor(
+		private readonly loggerService: LoggerService,
+		private readonly sprintService: SprintService
+	){}
+
+	@Post()
+	async createSprint(
+		@UserId() callerId: string,
+		@Param('projectId') projectId: string,
+		@Body() createSprintObject: CreateSprintDto
+	){
+		this.loggerService.log(`Create Sprint by caller: ${callerId}`, this.context);
+	}
+
+	@Get()
+	async getAllSprint(
+		@UserId() callerId: string,
+		@Param('projectId') projectId: string,
+	){
+		this.loggerService.log(`Get All Sprint by caller: ${callerId}`, this.context);
+	}
+
+	@Get(':sprintId')
+	async getSprint(
+		@UserId() callerId: string,
+		@Param('projectId') projectId: string,
+		@Param('sprintId') sprintId: string
+	){
+		this.loggerService.log(`Get Specific Sprint by caller: ${callerId}`, this.context);
+	}
+
+	@Patch(':sprintId')
+	async updateSprint(
+		@UserId() callerId: string,
+		@Param('projectId') projectId: string,
+		@Param('sprintId') sprintId: string,
+		@Body() updateSprintObject: UpdateSprintDto
+	){
+		this.loggerService.log(`Update Sprint by caller: ${callerId}`, this.context);
+	}
+
+	@Post(':sprintId/start')
+	async startSprint(
+		@UserId() callerId: string,
+		@Param('projectId') projectId: string,
+		@Param('sprintId') sprintId: string,
+		@Body() startSprintObject: StartSprintDto
+	){
+		this.loggerService.log(`Start Sprint by caller: ${callerId}`, this.context);
+	}
+
+	@Post(':sprintId/complete')
+	async completeSprint(
+		@UserId() callerId: string,
+		@Param('projectId') projectId: string,
+		@Param('sprintId') sprintId: string,
+		@Body() completeSprintObject: CompleteSprintDto
+	){
+		this.loggerService.log(`Complete Sprint by caller: ${callerId}`, this.context);
+	}
+
+	@Delete(':sprintId')
+	async deleteSprint(
+		@UserId() callerId: string,
+		@Param('projectId') projectId: string,
+		@Param('sprintId') sprintId: string
+	){
+		this.loggerService.log(`Delete Sprint by caller: ${callerId}`, this.context);
+	}
+
+	@Get(':sprintId/issues')
+	async getSprintIssues(
+		@UserId() callerId: string,
+		@Param('projectId') projectId: string,
+		@Param('sprintId') sprintId: string
+	){
+		this.loggerService.log(`Get Issues Of Sprint by caller: ${callerId}`, this.context);
+	}
+}
